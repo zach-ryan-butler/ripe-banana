b17938463262dc7387547611c004d5a442f514f1
Zach Butler <zach.standley93@gmail.com>
2019-07-19 17:42:00 -0700

    updated studios get by id route and test

--- test/studios.test.js
+++ test/studios.test.js
@@ -5,6 +5,8 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Studio = require('../lib/models/studio');
+const Film = require('../lib/models/film');
+const Actor = require('../lib/models/actor');
 
 describe('studio routes', () => {
   beforeAll(() => {
@@ -62,8 +64,51 @@ describe('studio routes', () => {
       }
     });
 
+    const actor = await Actor.create({
+      name: 'zach',
+      dob: '11-14-2010',
+      pob: 'Oregon'
+    });
+
+    const film = await Film.create({
+      title: 'Fight Club', 
+      studio: studio._id, 
+      released: '1999', 
+      cast: [{ role: 'bill', actor: actor._id }]
+      });
+      
+
     return request(app)
       .get(`/api/v1/studios/${studio._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Fox 2000 Pictures',
+          address: {
+            city: 'Los Angeles',
+            state: 'California',
+            country: 'United States'
+          },
+          films: [{
+            _id: film._id.toString(),
+            title:'Fight Club'
+          }]
+        });
+      });
+  });
+
+  it('can delete a studio by id if there are no films', async() => {
+    const studio = await Studio.create({
+      name: 'Fox 2000 Pictures',
+      address: {
+        city: 'Los Angeles',
+        state: 'California',
+        country: 'United States'
+      }
+    });
+
+    return request(app)
+      .delete(`/api/v1/studios/${studio._id}`)
       .then(res => {
         const studioJSON = JSON.parse(JSON.stringify(studio));
         expect(res.body).toEqual(studioJSON);
